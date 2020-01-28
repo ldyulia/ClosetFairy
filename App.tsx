570044ec686434bd8a42e3c6776154ebef52fd5e
ldyulia <leejulia.dy@gmail.com>
2020-01-28 12:31:36 -0500

    - make json files by country name

--- App.tsx
+++ App.tsx
@@ -1,19 +1,22 @@
 import * as React from 'react';
-import {View, Text, ActivityIndicator} from 'react-native';
+import {View, Text} from 'react-native';
 import Geolocation from 'react-native-geolocation-service';
 import styled from 'styled-components/native';
 import Loader from './components/utils/Loader';
+// import cityKR from './APIs/city.kr.json';
+import cityUS from './APIs/city.us.json';
 
 export interface AppProps {}
 
 const App: React.SFC<AppProps> = () => {
   const [isLoading, setLoading] = React.useState(true);
-  const [position, setPosition] = React.useState(null);
+  const [position, setPosition] = React.useState({});
 
   console.log(position);
 
   React.useEffect(() => {
     _getPosition();
+    // console.log('cityList: ', cityList);
   }, []);
 
   const _getPosition = () => {
@@ -22,6 +25,7 @@ const App: React.SFC<AppProps> = () => {
         // console.log('position: ', position);
         setLoading(false);
         setPosition(position);
+        console.log(cityUS);
       },
       error => {
         console.log(error.code, error.message);
